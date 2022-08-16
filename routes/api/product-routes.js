adff78423d7e3b2a3b7ccd66eaf892cb0b9a102a
Auggie <miaugus@gmail.com>
2022-08-16 18:00:10 -0400

    routes should be good

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -125,8 +125,22 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(400).json({ message: "Product does not exist" });
+      return;
+    }
+    res.status(200).json({ message: "Product deleted" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
